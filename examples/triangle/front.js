6f1ef77a8a12cf3e107ae895b64b9f5feff94781
Mathieu Lecarme <mathieu@garambrogne.net>
2012-02-12 23:26:43 +0100

    Example with the new API.

--- examples/triangle/front.js
+++ examples/triangle/front.js
@@ -1,5 +1,4 @@
-var redis = require('redis'),
-    connect = require('connect'),
+var http = require('http'),
     cluster_lib = require('../../lib/cluster');
 
 //front and dispatch computer
@@ -9,27 +8,8 @@ var redis = require('redis'),
 // cache job result
 // handle missed response.
 
-var cluster = cluster_lib.createCluster(function() {
-    var web = connect.createServer(
-        connect.favicon(),
-
-        connect.router(function(app) {
-            app.get('/', function(req, res, next) {
-                res.writeHead(200, {'Content-Type': 'text/plain'});
-                //put task to the queue.
-                var job_id = cluster.work('working', 'something_long',
-                    ['hello', 'world'], cluster.self(), function(err, resp) {
-                    });
-                //the answer is back, lets write it.
-                cluster.on('id:something_long:' + job_id, function(arg) {
-                    res.write(arg[0]);
-                    res.end('\n');
-                });
-            });
-        })
-        );
-
-    web.listen(1337, '127.0.0.1');
-
+cluster_lib.createCluster(function() {
+    http.createServer(cluster_lib.http.work(this, 'tempest')).
+        listen(1337, '127.0.0.1');
 });
 
--- examples/triangle/worker.js
+++ examples/triangle/worker.js
@@ -3,12 +3,14 @@ var cluster_lib = require('../../lib/cluster');
 //Working computer
 
 var cluster = cluster_lib.createCluster();
-cluster.queues.push('working');
+cluster.queues.push('tempest');
+var cpt = 0;
 
-cluster.worker.on('something_long', function(args, respond_to, job_id) {
+cluster.worker.on('url', function(args, respond_to, job_id) {
+    cpt++;
     //got a new task to do, lets answer
-    cluster.answer(respond_to, 'something_long', job_id, ['Hello world!'],
-        function(err, resp) { });
+    cluster.answer(respond_to, 'url', job_id,
+        [200, {}, 'Hello world! #' + cpt], function(err, resp) { });
 });
 
 cluster.work_loop();
