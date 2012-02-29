58807bdbef02840375cf7035e6ff45a16e89d481
Mathieu Lecarme <mathieu@garambrogne.net>
2012-03-01 00:06:53 +0100

    Default queue for web workers.

--- examples/triangle/worker.js
+++ examples/triangle/worker.js
@@ -3,14 +3,16 @@ var cluster_lib = require('../../lib/cluster');
 //Working computer
 
 var cluster = cluster_lib.createCluster();
-cluster.queues.push('tempest');
 var cpt = 0;
 
 cluster.worker.on('url', function(args, respond_to, job_id) {
     cpt++;
     //got a new task to do, lets answer
-    cluster.answer(respond_to, 'url', job_id,
-        [200, {}, 'Hello world! #' + cpt], function(err, resp) { });
+    cluster.answer(respond_to, 'url', job_id, [
+        200, // http code
+        {}, // headers
+        'Hello world! #' + cpt // body
+       ]);
 });
 
 cluster.work_loop();
